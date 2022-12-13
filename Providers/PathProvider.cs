01108e68bbec23de67f48b2d921f94f468ea111c
siswoo <55102664+siswoo@users.noreply.github.com>
2022-12-13 17:37:00 -0500

    Add files via upload

---
+++ Providers/PathProvider.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace CRUDHoteles.Providers
+{
+    public enum Folders
+    {
+        Fotos = 0
+    }
+
+    public class PathProvider
+    {
+        private IWebHostEnvironment hostEnvironment;
+
+        public PathProvider(IWebHostEnvironment hostEnvironment) { 
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public string MapPath(string fileName, Folders folder)
+        {
+            string carpeta = "";
+            if(folder == Folders.Fotos)
+            {
+                carpeta = "Fotos";
+            }
+
+            string patch = Path.Combine(this.hostEnvironment.WebRootPath, carpeta, fileName);
+
+            //if(folder == Folders.Temp)
+            //{
+            //    patch = Path.Combine(Path.GetFullPath(), fileName);
+            //}
+            return patch;
+        }
+    }
+}
