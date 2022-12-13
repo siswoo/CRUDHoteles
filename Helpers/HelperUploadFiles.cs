7165e3f75bf14debdf24161125ff103320a75df0
siswoo <55102664+siswoo@users.noreply.github.com>
2022-12-13 17:36:26 -0500

    Add files via upload

---
+++ Helpers/HelperUploadFiles.cs
@@ -0,0 +1,26 @@
+﻿using CRUDHoteles.Providers;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace CRUDHoteles.Helpers
+{
+    public class HelperUploadFiles
+    {
+        private PathProvider pathProvider;
+
+        public HelperUploadFiles(PathProvider pathProvider)
+        {
+            this.pathProvider = pathProvider;
+        }
+
+        public async Task<String> UploadFilesAsync(IFormFile formFile, string nombreImagen, Folders folder)
+        {
+            string path = this.pathProvider.MapPath(nombreImagen, folder);
+            using (Stream stream = new FileStream(path, FileMode.Create))
+            {
+                await formFile.CopyToAsync(stream);
+            }
+            return path;
+        }
+    }
+}
